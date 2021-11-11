3e6cf3310906e85e712c0a19c96c9b72c484e665
NicolaL95 <nicola.lilla@gmail.com>
2021-11-11 16:59:39 +0100

    Fine struttura footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer id="site_footer">
+    <div class="button_container footer_format">
+      <button>SIGN-UP NOW!</button>
+    </div>
+    <nav class="link_container footer_format">
+      <a href=""><img src="../assets/footer-facebook.png" alt="" /></a>
+      <a href=""><img src="../assets/footer-twitter.png" alt="" /></a>
+      <a href=""><img src="../assets/footer-youtube.png" alt="" /></a>
+      <a href=""><img src="../assets/footer-pinterest.png" alt="" /></a>
+      <a href=""><img src="../assets/footer-periscope.png" alt="" /></a>
+    </nav>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+</style>
