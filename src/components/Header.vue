d6edb9b87d3dc42ae236a7bad13b6179d890b68b
NicolaL95 <nicola.lilla@gmail.com>
2021-11-11 15:31:47 +0100

    Fine struttura Header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header id="site_header">
+    <div class="img_container header_format">
+      <img src="../assets/dc-logo.png" alt="" />
+    </div>
+    <nav class="link_container header_format">
+      <a href="">CHARACTERS</a> | <a href="">COMICS</a> |
+      <a href="">MOVIES</a> | <a href="">TV</a> | <a href="">GAMES</a> |
+      <a href="">COLLECTIBLES</a> | <a href="">VIDEOS</a> |
+      <a href="">FANS</a> | <a href="">NEWS</a> | <a href="">SHOP</a> |
+    </nav>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+#site_header {
+  display: flex;
+  .header_format {
+    width: 50%;
+  }
+}
+</style>
