40d950ece8cdf53368bd9ddee88bbfc9ad9cce0a
NicolaL95 <nicola.lilla@gmail.com>
2021-11-11 16:49:23 +0100

    Fine struttura Main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,4 +1,83 @@
-<template></template>
+<template>
+  <main id="site_main">
+    <div class="content">
+      <p class="content_here">Content goes here</p>
+    </div>
+    <div class="generic_container">
+      <div class="generic_content">
+        <img src="../assets/buy-comics-digital-comics.png" alt="" />
+        <p>Digital Comics</p>
+      </div>
+      <div class="generic_content">
+        <img src="../assets/buy-comics-merchandise.png" alt="" />
+        <p>DC MERCHANDISE</p>
+      </div>
+      <div class="generic_content">
+        <img src="../assets/buy-comics-subscriptions.png" alt="" />
+        <p>SUBSCRIPTION</p>
+      </div>
+      <div class="generic_content">
+        <img src="../assets/buy-comics-shop-locator.png" alt="" />
+        <p>COMIC SHOP LOCATOR</p>
+      </div>
+      <div class="generic_content">
+        <img src="../assets/buy-dc-power-visa.svg" alt="" />
+        <p>DC POWER VISA</p>
+      </div>
+    </div>
+    <div class="dc_table">
+      <table>
+        <thead>
+          <tr>
+            <th>DC COMICS</th>
+            <th>SHOP</th>
+            <th>DC</th>
+            <th>SITES</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Characters</td>
+            <td>Comics</td>
+            <td>Movies</td>
+            <td>TV</td>
+            <td>Games</td>
+            <td>Videos</td>
+            <td>News</td>
+          </tr>
+          <tr>
+            <td>Shop DC</td>
+            <td>Shop DC Coollectibles</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Terms Of Use</td>
+            <td>Privacy policy (New)</td>
+            <td>Ad Choices</td>
+            <td>Advertising</td>
+            <td>Jobs</td>
+            <td>Subscription</td>
+            <td>Talent Workshops</td>
+            <td>CPSC Certificates</td>
+            <td>Ratings</td>
+            <td>Shop Help</td>
+            <td>Coontact Us</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>DC</td>
+            <td>MAD Magazine</td>
+            <td>DC kids</td>
+            <td>DC Universe</td>
+            <td>DC Power Visa</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
+</template>
 
 <script>
 </script>
